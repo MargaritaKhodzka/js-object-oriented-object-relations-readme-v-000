aed319fc0f8905d9b2cc5e2408e3b6fdda0bfcbb
MargaritaKhodzka <mkhodzka@gmail.com>
2018-03-13 22:02:34 -0400

    wrote methods that select associated data

--- object-relations.js
+++ object-relations.js
@@ -1,8 +1,6 @@
 let itemId = 0
-let store = {
-  items: [],
-  users: []
-}
+let userId = 0
+let store = {items: [], users: []}
 
 class Item {
   constructor (price, name, user) {
@@ -12,11 +10,17 @@ class Item {
     if (user) {
       this.userId = user.id
     }
-
+    //insert the item into the store
     store.items.push(this)
   }
   setUser (user) {
     this.userId = user.id
+  }
+
+  user () {
+    return store.users.find (function (user) {
+      return user.id === this.userId
+    })
   }
 }
 
@@ -25,11 +29,26 @@ class User {
     this.id = ++userId
     this.name = name
 
+    //insert the user into the store
     store.users.push(this)
+  }
+  items () {
+    return store.items.filter (item => {
+      return item.userId === this.id
+    })
   }
 }
 
-let bobby = new User('bobby')
+let bobby = new User('Bobby')
+let sally = new User('Sally')
+let user = new User('Freddie')
+
 let trousers = new Item (24, 'trousers', bobby)
+let tshirt = new Item(8, 'tshirt', bobby)
+let socks = new Item(3, 'socks', sally)
+let item = new Item(3, 'socks', freddie)
+
+purpleSocks.freddie()
+
 
 store
